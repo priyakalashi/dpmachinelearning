bf4d81620305978c4f6e2dd428ee7b28fc18ae36
Priya <43725307+priyakalashi@users.noreply.github.com>
2026-04-25 15:31:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,29 @@ with st.sidebar:
   st.sidebar.markdown("""
 [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
 """)
-  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
-  body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male','female'))
+  uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+  if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+  else:
+    island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+    bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
+    bill_depth_mm = st.slider('Bill Depth (mm)', 13.1, 21.5, 17.2)
+    flipper_length_mm = st.slider('Flipper Length (mm)', 172.0, 231.0, 201.0)
+    body_mass_g = st.slider('Body Mass (g)', 2700.0, 6300.0, 4207.0)
+    gender = st.selectbox('Gender', ('male','female'))
 
 
 # create dataframe from the input
-  data = {
-    'island' : island,
-    'bill_length_mm' : bill_length_mm,
-    'bill_depth_mm' : bill_depth_mm,
-    'flipper_length_mm' : flipper_length_mm,
-    'body_mass_g' : body_mass_g,
-    'sex' : gender
-  }
-  input_df = pd.DataFrame(data,index=[0])
-  input_penguins = pd.concat([input_df,X_raw], axis=0)
+    data = {
+      'island' : island,
+      'bill_length_mm' : bill_length_mm,
+      'bill_depth_mm' : bill_depth_mm,
+      'flipper_length_mm' : flipper_length_mm,
+      'body_mass_g' : body_mass_g,
+      'sex' : gender
+    }
+    input_df = pd.DataFrame(data,index=[0])
+    input_penguins = pd.concat([input_df,X_raw], axis=0)
 
 #encode cat column
 encode = ['island','sex']
